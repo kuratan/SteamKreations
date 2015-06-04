f7ce0ef8e7939bf842c568d563337cb0c0724a2f
Kuraton <kuraron.kuratan@gmail.com>
2015-06-04 23:21:32 +0200

    Try some basic usage of IFluidHandler and TileFluidHandler

--- src/main/java/de/kuratan/steamkreations/tileentity/TileEntitySteamer.java
+++ src/main/java/de/kuratan/steamkreations/tileentity/TileEntitySteamer.java
@@ -1,8 +1,22 @@
 package de.kuratan.steamkreations.tileentity;
 
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.IFluidHandler;
 import net.minecraftforge.fluids.TileFluidHandler;
 
 public class TileEntitySteamer extends TileFluidHandler implements IFluidHandler {
 
+    @Override
+    public boolean canDrain(ForgeDirection from, Fluid fluid) {
+        return false;
+    }
+
+    @Override
+    public boolean canFill(ForgeDirection from, Fluid fluid) {
+        if (from.equals(ForgeDirection.DOWN)) {
+            return super.canFill(from, fluid);
+        }
+        return false;
+    }
 }
