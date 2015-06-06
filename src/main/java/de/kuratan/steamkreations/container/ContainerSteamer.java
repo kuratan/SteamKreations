14b350c93a30d1563b42cdfc37949e2a391d3764
Kuraton <kuraron.kuratan@gmail.com>
2015-06-06 02:00:27 +0200

    better dummy gui

--- src/main/java/de/kuratan/steamkreations/container/ContainerSteamer.java
+++ src/main/java/de/kuratan/steamkreations/container/ContainerSteamer.java
@@ -14,24 +14,26 @@ public class ContainerSteamer extends Container {
 
     public ContainerSteamer(final InventoryPlayer inventoryPlayer, final TileEntitySteamer tileEntitySteamer) {
         this.tileEntitySteamer = tileEntitySteamer;
-        this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 0, 30, 40));
         if (tileEntitySteamer.getType() != TileEntitySteamer.TYPES.NORMAL) {
-            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 1, 30, 80));
-            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 2, 70, 40));
-            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 3, 70, 80));
+            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 0, 3 + 4 * 18, 17 + 18));
+            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 1, 3 + 5 * 18, 17 + 18));
+            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 2, 3 + 4 * 18, 17 + 2 * 18));
+            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 3, 3 + 5 * 18, 17 + 2 * 18));
+        } else {
+            this.addSlotToContainer(new SlotSteamer(tileEntitySteamer, 0, 12 + 4 * 18, 8 + 2 * 18));
         }
 
         for (int inventoryRowIndex = 0; inventoryRowIndex < 3; ++inventoryRowIndex)
         {
             for (int inventoryColumnIndex = 0; inventoryColumnIndex < 9; ++inventoryColumnIndex)
             {
-                this.addSlotToContainer(new Slot(inventoryPlayer, inventoryColumnIndex + inventoryRowIndex * 9 + 9, 8 + inventoryColumnIndex * 18, 94 + inventoryRowIndex * 18));
+                this.addSlotToContainer(new Slot(inventoryPlayer, inventoryColumnIndex + inventoryRowIndex * 9 + 9, 12 + inventoryColumnIndex * 18, 174 - (4 - inventoryRowIndex) * 18));
             }
         }
 
         for (int actionBarSlotIndex = 0; actionBarSlotIndex < 9; ++actionBarSlotIndex)
         {
-            this.addSlotToContainer(new Slot(inventoryPlayer, actionBarSlotIndex, 8 + actionBarSlotIndex * 18, 152));
+            this.addSlotToContainer(new Slot(inventoryPlayer, actionBarSlotIndex, 12 + actionBarSlotIndex * 18, 160));
         }
     }
 
--- src/main/java/de/kuratan/steamkreations/gui/GuiSteamer.java
+++ src/main/java/de/kuratan/steamkreations/gui/GuiSteamer.java
@@ -14,12 +14,14 @@ public class GuiSteamer extends GuiContainer {
     public GuiSteamer(final InventoryPlayer inventoryPlayer, final TileEntitySteamer tileEntitySteamer) {
         super(new ContainerSteamer(inventoryPlayer, tileEntitySteamer));
         this.tileEntitySteamer = tileEntitySteamer;
+        this.xSize = 184;
+        this.ySize = 184;
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(new ResourceLocation(SteamKreations.MOD_ID, "textures/gui/steamer.png"));
+        this.mc.getTextureManager().bindTexture(new ResourceLocation(SteamKreations.MOD_ID, this.tileEntitySteamer.getType()== TileEntitySteamer.TYPES.NORMAL?"textures/gui/steamer.png":"textures/gui/steamer_big.png"));
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
         drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
